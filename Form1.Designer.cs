3d776738ec9f3d9b0bccdd95f7b7036c8581bbb1
PingWinning <simeonov02@yahoo.com>
2025-10-04 16:14:10 -0400

    [CRÉATION] Code initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ListeProduits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblPrix = new System.Windows.Forms.Label();
+            this.txtBoxNom = new System.Windows.Forms.TextBox();
+            this.numPrix = new System.Windows.Forms.NumericUpDown();
+            this.listBoxProduits = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numNbProduits = new System.Windows.Forms.NumericUpDown();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNbProduits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Produit";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNom.Location = new System.Drawing.Point(39, 68);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(36, 16);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "Nom";
+            // 
+            // lblPrix
+            // 
+            this.lblPrix.AutoSize = true;
+            this.lblPrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrix.Location = new System.Drawing.Point(39, 106);
+            this.lblPrix.Name = "lblPrix";
+            this.lblPrix.Size = new System.Drawing.Size(29, 16);
+            this.lblPrix.TabIndex = 2;
+            this.lblPrix.Text = "Prix";
+            // 
+            // txtBoxNom
+            // 
+            this.txtBoxNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxNom.Location = new System.Drawing.Point(149, 63);
+            this.txtBoxNom.Name = "txtBoxNom";
+            this.txtBoxNom.Size = new System.Drawing.Size(276, 24);
+            this.txtBoxNom.TabIndex = 3;
+            // 
+            // numPrix
+            // 
+            this.numPrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numPrix.Location = new System.Drawing.Point(149, 102);
+            this.numPrix.Name = "numPrix";
+            this.numPrix.Size = new System.Drawing.Size(276, 24);
+            this.numPrix.TabIndex = 4;
+            // 
+            // listBoxProduits
+            // 
+            this.listBoxProduits.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxProduits.FormattingEnabled = true;
+            this.listBoxProduits.ItemHeight = 18;
+            this.listBoxProduits.Location = new System.Drawing.Point(42, 159);
+            this.listBoxProduits.Name = "listBoxProduits";
+            this.listBoxProduits.Size = new System.Drawing.Size(383, 130);
+            this.listBoxProduits.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre de produits";
+            // 
+            // numNbProduits
+            // 
+            this.numNbProduits.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNbProduits.Location = new System.Drawing.Point(369, 328);
+            this.numNbProduits.Name = "numNbProduits";
+            this.numNbProduits.ReadOnly = true;
+            this.numNbProduits.Size = new System.Drawing.Size(56, 24);
+            this.numNbProduits.TabIndex = 7;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouter.Location = new System.Drawing.Point(449, 159);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(175, 31);
+            this.btnAjouter.TabIndex = 8;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupprimer.Location = new System.Drawing.Point(449, 211);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(175, 31);
+            this.btnSupprimer.TabIndex = 9;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEffacer.Location = new System.Drawing.Point(449, 261);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(175, 31);
+            this.btnEffacer.TabIndex = 10;
+            this.btnEffacer.Text = "Effacer";
+            this.btnEffacer.UseVisualStyleBackColor = true;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitter.Location = new System.Drawing.Point(449, 325);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(175, 31);
+            this.btnQuitter.TabIndex = 11;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 378);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnEffacer);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.numNbProduits);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxProduits);
+            this.Controls.Add(this.numPrix);
+            this.Controls.Add(this.txtBoxNom);
+            this.Controls.Add(this.lblPrix);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Laboratoire de révision";
+            ((System.ComponentModel.ISupportInitialize)(this.numPrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numNbProduits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblPrix;
+        private System.Windows.Forms.TextBox txtBoxNom;
+        private System.Windows.Forms.NumericUpDown numPrix;
+        private System.Windows.Forms.ListBox listBoxProduits;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numNbProduits;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.Button btnQuitter;
+    }
+}
+
